4a120ce43bb8168de05f4e2f303f213a2bbb829b
Dongwoon Son <dongwoon.son@dreamus.io>
2023-08-28 14:25:50 +0900

    Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # jdbc-cli-demo
+
+## Environment Variables
+
+* `DB_URL`
+    * Example: `jdbc:mysql://host.domain:3306/database-name`
+* `DB_USER`
+* `DB_PASSWORD`
--- app/src/main/java/com/dreamuscompany/flo/bernard/jdbccli/demo/App.java
+++ app/src/main/java/com/dreamuscompany/flo/bernard/jdbccli/demo/App.java
@@ -2,60 +2,91 @@ package com.dreamuscompany.flo.bernard.jdbccli.demo;
 
 import software.aws.rds.jdbc.mysql.shading.com.mysql.cj.jdbc.MysqlDataSource;
 
+import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class App {
-    public static void main(String[] args) {
 
-        String url = "jdbc:mysql://<HOST>:<PORT>/<DB>>";
-        String user = "<USER>";
-        String password = "<PASSWORD>";
+    public static void main(String[] args) throws Exception {
 
         System.out.println("start");
 
-        try {
-            MysqlDataSource mysqlDataSource = new MysqlDataSource();
-            mysqlDataSource.setURL(url);
-            mysqlDataSource.setUser(user);
-            mysqlDataSource.setPassword(password);
+        var dataSource = createDataSource();
 
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()
+        ) {
+            connection.setAutoCommit(false);
 
-            try (Connection connection = mysqlDataSource.getConnection();
-                 Statement statement = connection.createStatement()
-            ) {
-                connection.setAutoCommit(false);
-                System.out.println("connected");
+            System.out.println("connected");
 
-                var reader = new BufferedReader(new InputStreamReader(System.in) );
+            var stdinReader = new BufferedReader(new InputStreamReader(System.in));
 
-                while (true) {
-                    try {
-                        String line = reader.readLine();
-                        if (line == null || line.isBlank()) {
-                            continue;
-                        }
-
-                        boolean selected = statement.execute(line);
-                        if (selected) {
-                            ResultSet resultSet = statement.getResultSet();
-                            while (resultSet.next()) {
-                                System.out.println(resultSet.getString(1));
-                            }
-                            System.out.println("Done");
-                        } else {
-                            System.out.println(statement.getUpdateCount());
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            boolean cont;
+            do {
+                cont = readEvalPrint(stdinReader, statement);
+            } while (cont);
         }
+    }
+
+    private static boolean readEvalPrint(BufferedReader stdinReader, Statement statement) throws Exception {
+
+        var line = stdinReader.readLine();
+        if (line == null) {
+            return false;
+        }
+
+        if (line.isBlank()) {
+            return true;
+        }
+
+        try {
+
+            boolean hasResultSet = statement.execute(line);
+            if (hasResultSet) {
+                printResultSet(statement);
+            } else {
+                System.out.println("updated: " + statement.getUpdateCount());
+            }
+
+        } catch (SQLException e) {
+            System.out.println("error: " + e.getMessage());
+        }
+
+        return true;
+    }
+
+    private static void printResultSet(Statement statement) throws Exception {
+
+        try (ResultSet resultSet = statement.getResultSet()) {
+            var columnCount = resultSet.getMetaData().getColumnCount();
+
+            while (resultSet.next()) {
+                for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                    System.out.print(resultSet.getObject(columnIndex) + "\t");
+                }
+                System.out.println();
+            }
+        }
+
+        System.out.println("-- end --");
+    }
+
+    private static DataSource createDataSource() {
+
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setURL(url);
+        mysqlDataSource.setUser(user);
+        mysqlDataSource.setPassword(password);
+
+        return mysqlDataSource;
     }
 }
